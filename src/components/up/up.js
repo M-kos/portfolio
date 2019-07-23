a5bb713e71937aa3e1133565d65f5c72e800ae26
M-kos <koc.melnikov.1985@gmail.com>
2019-07-23 17:01:28 +0700

    Added links for social networking buttons, removed default buttons up

--- src/components/up/up.js
+++ src/components/up/up.js
@@ -1,6 +1,12 @@
 import UpScroll from './_scrolled/up_scrolled';
 
 function Up() {
+    let btnUp = document.querySelector('.up');
+    btnUp.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector("#home");
+        target.scrollIntoView();
+    });
 
     UpScroll();
 }
